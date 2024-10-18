5439301f16bbe9fef1d51530506105669f866a16
ASN-VIKING <vivek242007@gmail.com>
2024-10-18 15:19:22 +0530

    11

--- 1.js
+++ 1.js
@@ -1,1 +1,10 @@
-console.log("vivek here")
+let x=34
+let y=56
+console.table(["vivek here", 3+3,x+y])
+let z=1
+let boolan=Boolean(z)
+console.log(boolan)
+let num=456
+let storr=String(num)
+console.log(storr)
+console.log(typeof storr)
